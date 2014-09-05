f9145c1272bad116ddd9fe2b1865a99e402a9762
James Goppert <james.goppert@gmail.com>
2014-09-05 13:05:27 -0400

    Work on sympy utils.

--- sympy_utils.py
+++ sympy_utils.py
@@ -1,59 +1,67 @@
-import re
 import sympy
 
 
-class SympyDict(dict):
-    """
-    This saves a dictonary of sympy expressions to a file
-    in human readable form.
-    >>> a, b = sympy.symbols('a, b')
-    >>> d = SympyDict({'a':a, 'b':b})
-    >>> d.save('name.sympy')
-    >>> del d
-    >>> d2 = SympyDictd.load('name.sympy')
-    """
+def rhs_to_scipy_ode(rhs, t, x_vect, u_vect, constants, *args, **lambdify_kwargs):
+    """Convert rhs to lambda function and jacobian comaptible with scipy.
 
-    def __init__(self, *args, **kwargs):
-        super(SympyDict, self).__init__(*args, **kwargs)
-
-    def __repr__(self):
-        d = dict(self)
-        for key in d.keys():
-            d[key] = sympy.srepr(d[key])
-        # regex is just used here to insert a new line after
-        # each dict key, value pair to make it more readable
-        return re.sub('(: \"[^"]*\",)', r'\1\n',  d.__repr__())
-
-    def save(self, file):
-        with open(file, 'wb') as savefile:
-            savefile.write(self.__repr__())
-
-    @classmethod
-    def load(cls, file_path):
-        with open(file_path, 'r') as loadfile:
-            exec('d =' + loadfile.read())
-        d = locals()['d']
-        for key in d.keys():
-            d[key] = sympy.sympify(d[key])
-        return cls(d)
-
-
-def state_space_lambdify(f_vect, t, x_vect, u_vect, *args):
-    """
     Given a state space description of a dynamic system, create
-    a lambda funtion to simulate it with.
+    a lambda function with a signature compatible with
+    scipy.ode.integrate: f(t, x, u, *args).
 
-    Input
-        f_vect : sympy matrix of expressions for
-            the derivative of x (continuous),
-            or for the change in x (discrete)
-        t : the indenpendent time-like variable
-        x_vect : sympy matrix of symbols in x vector
-        u_vect : sympy matrix of symbols in u vector
-        *args: other parameters that are not states or inputs
+    Parameters
+    ----------
+    rhs : sympy.Matrix
+        Column vector of expressions for
+        the derivative of x (continuous),
+        or for the change in x (discrete).
+    t : sympy.Symbol
+        The independent time-like variable.
+    x_vect : sympy.Matrix
+        Column vector of symbols in the x (state) vector.
+    u_vect : sympy.Matrix
+        Column vector of symbols in the u (input) vector.
+        Most controllers pass a vector of inputs and this
+        supports this structure.
+    constants: dict
+        Dictionary of constants to substitute into rhs.
+    *args: dict
+        Additioinal symbols in f_vect that are not states, inputs
+        or constants.
+    **lambdify_kwargs:
+        Additional arugments to pass to lambdify.
+        @see sympy.utilities.lambdify.lambdify
+
+    Returns
+    -------
+    f : function
+        A lambda function that compute the right hand side
+        of the ode: f(t, x, u, *f_args).
+    jac : function
+        A lambda function that computes the jacobian
+        of the right hand side with respect to the state x:
+        jac(t, x, u, *jac_args).
     """
+    if constants is not None:
+        rhs = rhs.subs(constants)
     x = sympy.DeferredVector('x')
     u = sympy.DeferredVector('u')
     ss_subs = {x_vect[i]: x[i] for i in range(len(x_vect))}
     ss_subs.update({u_vect[i]: u[i] for i in range(len(u_vect))})
-    return sympy.lambdify((t, x, u) + args, f_vect.subs(ss_subs))
+    f = sympy.lambdify((t, x, u) + args, rhs.subs(ss_subs), **lambdify_kwargs)
+    jac_vect = rhs.jacobian(x_vect)
+    jac = sympy.lambdify((t, x, u) + args, jac_vect.subs(ss_subs))
+    return (f, jac)
+
+
+def save_sympy_expr(expr, filename):
+    with open(filename, 'wb') as save_file:
+        save_file.write(sympy.python(expr))
+
+
+def load_sympy_expr(filename):
+    with open(filename, 'r') as load_file:
+        load_string = load_file.read()
+    from sympy import *
+    exec(load_string)
+    e = locals()['e']
+    return e
